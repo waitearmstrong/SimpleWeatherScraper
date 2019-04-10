e75cd1038a82e671540811b05759ec2ee21bb023
Waite Armstrong <wha0001@mix.wvu.edu>
2019-04-10 09:46:52 -0400

    Update weather.py
    Added support for scraping multiple months at once. Improved the scraping algorithm to not crash when no data was recorded for a date.

--- weather.py
+++ weather.py
@@ -1,24 +1,48 @@
 import requests
+import calendar
 from bs4 import BeautifulSoup
-file = open("data.txt","a+")
-url = 'https://www.usclimatedata.com/climate/morgantown/west-virginia/united-states/uswv0507/2018/12'
-response = requests.get(url)
-soup = BeautifulSoup(response.content,'html.parser')
-table = soup.find('table', class_ = 'daily_climate_table')
-highs = []
-lows = []
-sumHighs = 0.0
-sumLows = 0.0
-for row in table.find_all("tr"):
-     for cell in row.find_all('td', class_ = 'temperature_red'):
-         highs.append(float(cell.text))
-for row in table.find_all("tr"):
-     for cell in row.find_all('td', class_ = 'temperature_blue'):
-         lows.append(float(cell.text))
-for num in highs:
-    sumHighs = sumHighs + num
-for num in lows:
-    sumLows = sumLows + num
-print("The average high is: " + str(sumHighs/len(highs)))
-print("The average low is: " + str(sumLows/len(lows)))
+url = 'https://www.usclimatedata.com/climate/morgantown/west-virginia/united-states/uswv0507/'
+urls = []
+count = 0
+while count < 12:
+    urls.append(url + "2017/" + str(count+1))
+    count+=1
+ccc = 0
+yearHighs = []
+yearLows = []
+for url in urls:
+    ccc +=1
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content,'html.parser')
+    table = soup.find('table', class_ = 'daily_climate_table')
+    highs = []
+    lows = []
+    sumHighs = 0.0
+    sumLows = 0.0
+    for row in table.find_all("tr"):
+         for cell in row.find_all('td', class_ = 'temperature_red'):
+             try:
+                 highs.append(float(cell.text))
+                 yearHighs.append(float(cell.text))
+             except:
+                 pass
 
+    for row in table.find_all("tr"):
+         for cell in row.find_all('td', class_ = 'temperature_blue'):
+             try:
+                 lows.append(float(cell.text))
+                 yearLows.append(float(cell.text))
+             except:
+                pass
+    for num in highs:
+        sumHighs = sumHighs + num
+    for num in lows:
+        sumLows = sumLows + num
+    print("The average low for " + calendar.month_name[ccc] + " is: " + str(sumLows/len(lows)))
+    print("The average high is: " + calendar.month_name[ccc] + " is: " + str(sumHighs/len(highs)))
+for num in yearHighs:
+        sumHighs = sumHighs + num
+for num in yearLows:
+        sumLows = sumLows + num
+print("The average low for the year is: " + str(sumLows/len(yearLows)))
+print("The average high for the year is: " + str(sumLows/len(yearHighs)))
